dff3165e3857a9219d5db6dcc0aa5962279f1b00
Himanshu Kumar <133102218+Himanshu16ky@users.noreply.github.com>
2024-03-02 22:23:50 +0530

    Update config.js

--- HimanshuKumar/backend/DB/config.js
+++ HimanshuKumar/backend/DB/config.js
@@ -1,2 +1,2 @@
 const monooose = require('mongoose');
-monooose.connect("mongodb://127.0.0.1:27017/store");
+monooose.connect("mongodb+srv://yadav16himanshu:XwIsB43o58y6GM1j@cluster0.rdk9bp5.mongodb.net/store");
